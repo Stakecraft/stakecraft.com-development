e20eba13533985f0fcdfa933e1e0819599d89161
“dorac-ext” <dora.cretu90@gmail.com>
2024-08-31 12:19:56 +0300

    Working switch theme

---
+++ stakecraftFE/src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import { RouterView } from 'vue-router'
+import { ref } from 'vue'
+export default {
+  components: { RouterView },
+  setup() {
+    const theme = ref('light')
+
+    setTimeout(() => {
+      theme.value = 'dark'
+      console.log(theme.value)
+    }, 1000)
+
+    return { theme }
+  }
+}
+</script>
+
+
+
+
+<template>
+  <van-config-provider :theme="theme">
+    <header></header>
+    <RouterView />
+  </van-config-provider>
+</template>
+
+
+<style>
+@import './main.scss';
+</style>
