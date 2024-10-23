e1449fa7ef1d64061783c6add44084e223545263
DoraCretu594118 <103416234+doracretu3pillar@users.noreply.github.com>
2024-10-23 21:41:50 +0300

    Update WhyChooseUs

--- stakecraftFE/src/components/Whychooseus.vue
+++ stakecraftFE/src/components/Whychooseus.vue
@@ -1,6 +1,29 @@
-<template></template>
+<template>
+  <div class="whychooseus mainAreas">
+    <div class="titleHeader">Why Choose Us?</div>
+    <div class="description">
+      Stakecraft is a team of geeks with vast experience in programming, developing, and managing
+      digital assets. We use top-notch infrastructure/hardware to run our nodes. Our team, with the
+      highest degree of responsibility, approaches management, timely installation of updates, and
+      monitoring safety of user funds. Join us and enjoy earnings from staking your digital assets.
+    </div>
+  </div>
+</template>
 <script>
 export default {
   setup() {}
 }
 </script>
+
+<style scoped>
+.whychooseus {
+  padding-top: 96px;
+  padding-bottom: 86px;
+}
+
+.description {
+  font-family: poppins;
+  font-size: 16px;
+  max-width: 740px;
+}
+</style>
