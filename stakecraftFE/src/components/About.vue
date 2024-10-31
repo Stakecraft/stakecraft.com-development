0ac83bbe62f0c5a00c45811a24dd4a601a11fd99
DoraCretu594118 <103416234+doracretu3pillar@users.noreply.github.com>
2024-10-31 13:50:59 +0200

    About - continue

--- stakecraftFE/src/components/About.vue
+++ stakecraftFE/src/components/About.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="mainAreas">
     <div class="about">
-      <div class="title">
-        <div class="titleHeader">About</div>
-        <div class="titleHeader">StakeCraft</div>
+      <div class="titleHeader">
+        <div>About</div>
+        <div>StakeCraft</div>
       </div>
       <div class="shortDescription">
         Our team has earned recognition in the crypto community as the reliable team that approaches
@@ -12,12 +12,14 @@
       <div class="ourCapabilities">
         <div class="title">Our Capabilities</div>
         <button
-          class="capabilitiesWrapper"
-          @click="openDescription(index)"
-          :class="{ withHeight: showDescription[index] }"
+          class="wrapper"
+          @click="openDescription(0)"
+          :class="{ withHeight: showDescription[0] }"
         >
-          <div class="capabilityTitle">Quality Environment & Security</div>
-          <button class="add" :class="{ around: showDescription[index] }">+</button>
+          <div class="presentation">
+            <div class="capabilityTitle">Quality Environment & Security</div>
+            <button class="add" :class="{ around: showDescription[0] }">+</button>
+          </div>
           <div class="capabilityDescription">
             A next-generation cross-chain oracle solution designed to boost the performance of smart
             contracts and blockchain apps.
@@ -25,12 +27,14 @@
         </button>
 
         <button
-          class="capabilitiesWrapper"
-          @click="openDescription(index)"
-          :class="{ withHeight: showDescription[index] }"
+          class="wrapper"
+          @click="openDescription(1)"
+          :class="{ withHeight: showDescription[1] }"
         >
-          <div class="capabilityTitle">Publicity</div>
-          <button class="add" :class="{ around: showDescription[index] }">+</button>
+          <div class="presentation">
+            <div class="capabilityTitle">Publicity</div>
+            <button class="add" :class="{ around: showDescription[1] }">+</button>
+          </div>
           <div class="capabilityDescription">
             A next-generation cross-chain oracle solution designed to boost the performance of smart
             contracts and blockchain apps.
@@ -38,12 +42,14 @@
         </button>
 
         <button
-          class="capabilitiesWrapper"
-          @click="openDescription(index)"
-          :class="{ withHeight: showDescription[index] }"
+          class="wrapper"
+          @click="openDescription(2)"
+          :class="{ withHeight: showDescription[2] }"
         >
-          <div class="capabilityTitle">Technical Support</div>
-          <button class="add" :class="{ around: showDescription[index] }">+</button>
+          <div class="presentation">
+            <div class="capabilityTitle">Technical Support</div>
+            <button class="add" :class="{ around: showDescription[index] }">+</button>
+          </div>
           <div class="capabilityDescription">
             A next-generation cross-chain oracle solution designed to boost the performance of smart
             contracts and blockchain apps.
@@ -104,14 +110,27 @@ export default {
 </script>
 
 <style scoped>
-.titleHeader {
-  margin-bottom: 0;
-  line-height: 72px;
+.titleHeader  > * {
+  font-weight: 600;
+}
+
+.titleHeader > *:last-child {
+  margin: 30px 0 31px 0;
+}
+
+button {
+  background-color: #fff;
+  border: none;
 }
 
 .mainAreas {
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
+}
+
+.about {
+  max-width: 581px;
 }
 
 .shortDescription {
@@ -121,11 +140,118 @@ export default {
   font-weight: 400;
 }
 
+.team {
+  max-width: 575.49px;
+  display: flex;
+}
+
 .ourCapabilities .title {
   color: #00beb5;
   line-height: 34px;
   font-size: 26px;
   font-family: generalSans;
   font-weight: 600;
+  margin: 45px 0 30px 0;
 }
+
+.ourCapabilities .wrapper {
+  height: 62px;
+  overflow: hidden;
+  margin-bottom: 18px;
+  transition: 0.3s linear height;
+}
+
+.ourCapabilities .wrapper.withHeight {
+  height: 145px;
+}
+
+.capabilityDescription {
+  text-align: left;
+  padding: 0px 20px 15px 20px;
+  font-size: 16px;
+  line-height: 24px;
+  font-family: poppins;
+}
+
+.ourCapabilities .presentation {
+  display: flex;
+  padding: 16px 20px 11px 20px;
+  font-family: poppins;
+  font-size: 20px;
+  line-height: 30px;
+  justify-content: space-between;
+}
+
+.add {
+  font-size: 24px;
+  align-self: flex-end;
+  transition: 0.3s linear transform;
+}
+
+.add.around {
+  transform: rotate(45deg);
+}
+
+.teamRow1 > *, .teamRow2 > * {
+  width: 272px;
+  height: 299px;
+  transition: 0.3s linear background;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  padding: 0 0 15px 23px;
+}
+
+.teamRow1 .member1 {
+  background-image: url('../assets/vasily.svg');
+  margin: 0 25px 25px 0;
+}
+
+.teamRow1 .member2 {
+  background-image: url('../assets/alexander.svg');
+}
+
+.teamRow2 .member1 {
+  background-image: url('../assets/stanislav.svg');
+  margin: 0 25px 25px 0;
+}
+
+.teamRow2 .member2 {
+  background-image: url('../assets/alexN.svg');
+}
+
+.teamRow1 .member1:hover {
+  background-image: url('../assets/vasilyHover.svg');
+}
+
+.teamRow1 .member2:hover {
+  background-image: url('../assets/alexanderHover.svg');
+}
+
+.teamRow2 .member1:hover {
+  background-image: url('../assets/stanislavHover.svg');
+}
+
+.teamRow2 .member2:hover {
+  background-image: url('../assets/alexNHover.svg');
+}
+
+.memberTitle {
+  font-family: poppins;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 26px;
+  color: #FFF;
+  margin-bottom: 4px;
+}
+
+.memberDescription {
+  font-family: poppins;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: #FFF;
+  width: 221.83px;
+}
+
 </style>
